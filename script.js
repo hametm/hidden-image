090b2fab1a8e0b829546b4e94b4e3907d819357c
Morgan Hamet <morganhamet@gmail.com>
2022-07-15 01:07:56 -0700

    Add cursor styles

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const background = document.querySelector(".background");
-const eraseButton = document.querySelector(".erase");
+const popButton = document.querySelector(".pop");
 const fillButton = document.querySelector(".fill");
 const resetButton = document.querySelector(".reset");
 const clearButton = document.querySelector(".clear");
-const cursorButton = document.querySelector(".cursor");
 
 const rainbowColors = ["#ec87b9", "#e75ea3", "#fbbd4e", "#fdd349", 
 "#8ed1b5", "#55cbcb", "#44b0c6", "757acd", "e272d5"];
@@ -13,63 +12,65 @@ const positions = ["scale(2) translateX(-20px)", "scale(2) translateX(20px)",
 
 fillBoard();
 
-const squares = document.querySelectorAll(".square");
-
+const balloons = document.querySelectorAll(".balloon");
 
 function fillBoard() {
     for (let i = 0; i < 150; i++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        setRandomColor(rainbowColors, square);
-        setRandomPosition(square);
-        background.appendChild(square);
+        const balloon = document.createElement("div");
+        balloon.classList.add("balloon");
+        setRandomColor(rainbowColors, balloon);
+        setRandomPosition(balloon);
+        background.appendChild(balloon);
     }
 }
 
-eraseButton.onclick = () => {
-    squares.forEach(square => {
-        square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "transparent";
-        })
+popButton.onclick = () => {
+    background.style.setProperty("cursor", "url(./images/pin.png), auto");
+    balloons.forEach(balloon => {
+        balloon.addEventListener("mouseover", () => {
+            balloon.style.backgroundColor = "transparent";
+        });
     });
 }
 
 fillButton.onclick = () => {
-    squares.forEach(square => {
-        square.addEventListener("mouseover", () => {
-            setRandomColor(rainbowColors, square);
+    background.style.setProperty("cursor", "url(./images/plus.png), auto");
+    balloons.forEach(balloon => {
+        balloon.addEventListener("mouseover", () => {
+            setRandomColor(rainbowColors, balloon);
         })
     });
 }
 
 resetButton.onclick = () => {
-    squares.forEach(square => {
-        setRandomColor(rainbowColors, square);
-        setRandomPosition(square);
+    balloons.forEach(balloon => {
+        balloon.style.setProperty("cursor", "auto");
+        setRandomColor(rainbowColors, balloon);
+        setRandomPosition(balloon);
     });
 }
 
 clearButton.onclick = () => {
-    squares.forEach(square => {
-        square.style.backgroundColor = "transparent";
+    balloons.forEach(balloon => {
+        balloon.style.setProperty("cursor", "auto");
+        balloon.style.backgroundColor = "transparent";
     });
 }
 
-cursorButton.onclick = () => {
-    
-}
-
-function setRandomColor(colors, square) {
+function setRandomColor(colors, balloon) {
     let randomColor = Math.floor(Math.random() * (colors.length - 2));
-    square.style.backgroundColor = colors[randomColor];
+    balloon.style.backgroundColor = colors[randomColor];
 }
 
-function setRandomPosition(square) {
+function setRandomPosition(balloon) {
     let randomPosition = Math.floor(Math.random() * (positions.length - 2));
-    square.style.transform = positions[randomPosition];
+    balloon.style.transform = positions[randomPosition];
 }
 
-
-
-
+// Doesn't work
+function resetMouseover() {
+    balloons.forEach(balloon => {
+        balloon.removeEventListener("mouseover", setRandomColor)
+    });
+}
 
