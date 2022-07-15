5fcf2f72bec1012884f6e82c00c71bc40619488e
Morgan Hamet <morganhamet@gmail.com>
2022-07-15 14:51:31 -0700

    Add images and improve balloon functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hidden-image
+<a href="https://www.flaticon.com/free-icons/pin" title="pin icons">Pin icon created by Pixel perfect - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/plus" title="plus icons">Plus icon created by Stockio - Flaticon</a>
--- script.js
+++ script.js
@@ -8,69 +8,89 @@ const rainbowColors = ["#ec87b9", "#e75ea3", "#fbbd4e", "#fdd349",
 "#8ed1b5", "#55cbcb", "#44b0c6", "757acd", "e272d5"];
 const positions = ["scale(2) translateX(-20px)", "scale(2) translateX(20px)", 
         "scale(2) translateY(-20px)", "scale(2) translateY(20px)"];
+const images = ["giraffe", "loch-ness-monster", "torii", "poseidon", "gandalf", "dragon-fruit"];
 
 
 fillBoard();
 
-const balloons = document.querySelectorAll(".balloon");
 
 function fillBoard() {
     for (let i = 0; i < 150; i++) {
         const balloon = document.createElement("div");
         balloon.classList.add("balloon");
-        setRandomColor(rainbowColors, balloon);
-        setRandomPosition(balloon);
+        getColor(rainbowColors, balloon);
+        getPosition(balloon);
         background.appendChild(balloon);
     }
+    getImage();
 }
 
 popButton.onclick = () => {
-    background.style.setProperty("cursor", "url(./images/pin.png), auto");
+    const balloons = document.querySelectorAll(".balloon");
+    // background.style.setProperty("cursor", "url(./images/pin.png), auto");
     balloons.forEach(balloon => {
         balloon.addEventListener("mouseover", () => {
             balloon.style.backgroundColor = "transparent";
+            balloon.style.transform = "scale(.5)";
         });
     });
 }
 
 fillButton.onclick = () => {
-    background.style.setProperty("cursor", "url(./images/plus.png), auto");
-    balloons.forEach(balloon => {
-        balloon.addEventListener("mouseover", () => {
-            setRandomColor(rainbowColors, balloon);
-        })
-    });
+    // background.style.setProperty("cursor", "url(./images/plus.png), auto");
+    let balloonsExist = document.querySelector(".balloon");
+    if (balloonsExist) {
+        const balloons = document.querySelectorAll(".balloon");
+        balloons.forEach(balloon => {
+            balloon.addEventListener("mouseover", () => {
+                balloon.style.transform = "scale(2)";
+                getPosition(balloon);
+                getColor(rainbowColors, balloon);
+            })
+        });
+    } else {
+        for (let i = 0; i < 150; i++) {
+            const balloon = document.createElement("div");
+            balloon.classList.add("balloon");
+            balloon.style.backgroundColor = "transparent";
+            balloon.addEventListener("mouseover", () => {
+                getColor(rainbowColors, balloon);
+            });
+            getPosition(balloon);
+            background.appendChild(balloon);
+        }
+    }
 }
 
 resetButton.onclick = () => {
-    balloons.forEach(balloon => {
-        balloon.style.setProperty("cursor", "auto");
-        setRandomColor(rainbowColors, balloon);
-        setRandomPosition(balloon);
-    });
+    clearBoard();
+    fillBoard();
 }
 
 clearButton.onclick = () => {
-    balloons.forEach(balloon => {
-        balloon.style.setProperty("cursor", "auto");
-        balloon.style.backgroundColor = "transparent";
-    });
+    clearBoard();
 }
 
-function setRandomColor(colors, balloon) {
-    let randomColor = Math.floor(Math.random() * (colors.length - 2));
+function getColor(colors, balloon) {
+    let randomColor = Math.floor(Math.random() * (colors.length - 1));
     balloon.style.backgroundColor = colors[randomColor];
 }
 
-function setRandomPosition(balloon) {
-    let randomPosition = Math.floor(Math.random() * (positions.length - 2));
+function getPosition(balloon) {
+    let randomPosition = Math.floor(Math.random() * (positions.length));
     balloon.style.transform = positions[randomPosition];
 }
 
-// Doesn't work
-function resetMouseover() {
+function getImage() {
+    let randomImage = Math.floor(Math.random() * (images.length));
+    image = images[randomImage];
+    background.style.backgroundImage = `url(./images/${image}.png)`;
+}
+
+function clearBoard() {
+    const balloons = document.querySelectorAll(".balloon");
     balloons.forEach(balloon => {
-        balloon.removeEventListener("mouseover", setRandomColor)
+        balloon.remove();
     });
 }
 
